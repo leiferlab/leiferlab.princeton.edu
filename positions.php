bf20cbf4a40e9be472197a8d5095e4a9ca19152e
Andrew Leifer <andrew.leifer@gmail.com>
2012-10-29 16:08:11 -0400

    added p

--- positions.php
+++ positions.php
@@ -14,16 +14,16 @@
 	<hr />
 
 <h3>Post-docs</h3>
-Exceptional post-doctoral candidates should send a CV and two letters of reference.
+<p>Exceptional post-doctoral candidates should send a CV and two letters of reference.</p>
 
 <h3>Graduate Students</h3>
-Current or incoming Princeton graduate students interested in a rotation should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting.
+<p>Current or incoming Princeton graduate students interested in a rotation should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting.</p>
 
 <h3>Undergraduates</h3>
-Princeton undergraduates interested in conducting research in the summer, during the school year or as part of a Junior or Senior Thesis should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st.
+<p>Princeton undergraduates interested in conducting research in the summer, during the school year or as part of a Junior or Senior Thesis should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st.</p>
 
 <h3>Post-Bacs</h3>
-Send a CV and references to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a>.
+<p>Send a CV and references to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a>.</p>
 
 	</section>
 
