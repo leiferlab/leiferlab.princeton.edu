a0fa76f5abe07bf94cd4f3607de60871d210b50b
Andrew Leifer <andrew.leifer@gmail.com>
2012-10-29 12:27:26 -0400

    footer

---
+++ php/footer.php
@@ -0,0 +1,61 @@
+	<!-- Footer
+	================================================== -->
+
+	<footer id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="span4">
+					<a href="http://www.princeton.edu" title="Princeton University">
+						<img alt="Princeton University" src="assets/PUsig2-158U-ww-os.png">
+					</a>
+				</div>
+				<div class="span4 social-networks">
+					<h3>Find us elsewhere</h3>
+					<p> 
+						<a class="social-network icon-twitter" href="https://twitter.com/AndrewLeifer"></a>
+				<!-- <a class="social-network icon-facebook"></a> -->
+					<!--	<a class="social-network icon-vimeo"></a> -->   
+						<a class="social-network icon-github" href="https://github.com/leiferlab"></a> 
+						<!-- <a class="social-network icon-linkedin"></a> --> 
+					</p>
+				</div>
+				<div class="span4 newsletter">
+					<a href="http://www.genomics.princeton.edu/">
+						<img src="assets/lewis_sigler_gray_transparent.png" alt="Lewis-Sigler Institute for Integrative Genomics">
+					</a>
+
+				</div> 
+			</div> <!-- end .row -->		
+		</div> <!-- end .container -->
+	</footer><!-- end #footer -->
+
+	<footer id="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="span4">
+					&copy; 2012 <a href="http://www.princeton.edu">Princeton University</a>
+				</div>
+				<div class="span4">
+					
+					<i class="icon-envelope"></i> <a href="mailto:leifer@princeton.edu" title="Send us email">leifer@princeton.edu</a> &nbsp; <i class="icon-phone"></i> (609) 258-2973 
+				</div>
+				<div class="span4">
+					<a href="http://www.genomics.princeton.edu/">Lewis-Sigler Institute for Integrative Genomics</a>
+					
+				</div>
+			</div> <!-- end .row -->		
+		</div> <!-- end .container -->
+	</footer><!-- end #footer-extra -->
+
+	<!-- Javascript - Placed at the end of the document so the pages load faster 
+	================================================== -->
+
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+	<script type="text/javascript" src="js/jquery.touchSwipe.js"></script>
+	<script type="text/javascript" src="js/jquery.hotkeys.min.js" charset='utf-8'></script>
+	<script type="text/javascript" src="js/jquery.equalheights.js" ></script>
+	<script type="text/javascript" src="js/functions.js"></script>
+	
+</body>
+</html>		
