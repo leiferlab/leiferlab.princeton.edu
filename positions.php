d655ac6f01099b2d0c4f78f15f620d993006697c
Andrew Leifer <andrew.leifer@gmail.com>
2012-10-29 16:03:42 -0400

    draft of positions page

--- positions.php
+++ positions.php
@@ -10,11 +10,24 @@
 	<div class="hero-unit">
 		<p>We're looking for talented researchers.</p> 
 	</div>
-	<p> Our research span </p>
-
+	<p>Our work spans many disciplines to push the boundaries of optical neurophysiology. If you have a background in molecular biology, neuroscience, physics, engineering or programming and are interested in applying your skills towards learning how neural circuits drive behavior, then please contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> by email.	
 	<hr />
 
+<h3>Post-docs</h3>
+Exceptional post-doctoral candidates should send a CV and two letters of reference.
+
+<h3>Graduate Students</h3>
+Current or incoming Princeton graduate students interested in a rotation should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting.
+
+<h3>Undergraduates</h3>
+Princeton undergraduates interested in conducting research in the summer, during the school year or as part of a Junior or Senior Thesis should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st.
+
+<h3>Post-Bacs</h3>
+Send a CV and references to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a>.
+
 	</section>
+
+
 
 
 	<?php 
