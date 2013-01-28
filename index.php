c90042a842f22738f542c5e4a13e0896ef30b976
Andrew Leifer <leifer@princeton.edu>
2013-01-28 12:54:26 -0500

    broke into header and footer and updated front page

--- index.php
+++ index.php
@@ -4,147 +4,170 @@
 	include('php/header.php');
 ?>
 	
-	<section id="content" class="container">
+<section id="content" class="container"> <!-- This container consists of a giant row of text -->
+	<div>
+		<section class="row">
+			<article class="span12">
+				<h1><span class="grey">Optical</span> Neurophysiology of Behavior</h1>
+				<p>
+					Our lab seeks to understand how neural circuits encode complex behavior. In particular, we study the model organism <i>C. elegans</i>, a microscopic worm with a simple nervous system capable of rich behaviors. We have pioneered optogenetic microscopy techniques  to optically manipulate and monitor neural activity in worms as they move. <a class="" href="research.php">Read more &raquo;</a></p>
+			</article>
 
-</section><!-- end .container -->
-
-<div class="lead-container" id="frontpage">
-<div class="container">
-
-	<!-- Macbook carousel -->
-	<!-- Images should be 523 x 298 -->
-	<div id="carousel" class="carousel slide macbook" rel="carousel">
-		<img src="img/carousel-microscope.png" class="carousel-macbook" alt="" />
-		<!-- Carousel items -->
-		<div class="carousel-inner">
-			<!-- Item -->
-			<div class="item active">
-				<a href="#"><img src="assets/carousel/blueAVM.jpg" alt="" /></a>
-				<div class="carousel-caption">
-		        <p>To study neurocircuits, we build tools that monitor and manipulate neural activity in a worm with light.</p>
-		      </div>
-			</div>
-			<!-- end Item -->
-			
-			<!-- Item -->
-			<!--<div class="item">
-				<img src="assets/carousel/colbertSchematic.png" alt="" />
-				<div class="carousel-caption">
-			        <p>A high speed camera monitors the worm as it moves.</p>
-			    </div>
-			</div>-->
-			<!-- end Item -->			
-			
-			
-			<!-- Item -->
-			<div class="item">
-				<img src="assets/carousel/computerVision.png" alt="" />
-			<div class="carousel-caption">
-		        <p>Computer vision software identifies targets on the moving worm.</p>
-		      </div>
-			</div> 
-			<!-- end Item -->
-			
-			<!-- Item -->
-			<div class="item">
-				<img src="assets/carousel/greenOptics.jpg" alt="" />
-				<div class="carousel-caption">
-		        <p>Laser light is precisely targeted to neurons in the worm.</p>
-		      </div>
-			</div>
-			<!-- end Item -->
-			
-			
-			<!-- Item -->
-			<div class="item">
-				<img src="assets/carousel/inhibitNerveCord.png" alt="" />
-				<div class="carousel-caption">
-		        <p>Optogenetic proteins inhibit the worm's ventral nerve cord in response to green light. </p>
-		      </div>
-			</div>
-			<!-- end Item -->
-
-			<!-- Item -->
-			<div class="item">
-				<img src="assets/carousel/escapeCartoon.png" alt="" />
-				<div class="carousel-caption">
-			        <p>Changes to the worm's behavior are precisely quantified.</p>
-			    </div>
-			</div>
-			<!-- end Item -->			
-			
-			<!-- Item -->
-			<div class="item">
-				<img src="assets/carousel/circuit.png" alt="" />
-				<div class="carousel-caption">
-			        <p>These tools provide functional information about how neurons work together in a circuit.</p>
-			    </div>
-			</div>
-			<!-- end Item -->			
-			
-
-			
-			<!-- Item -->
-			<div class="item">
-				<img src="assets/carousel/calcium_imaging.png" alt="" />
-				<div class="carousel-caption">
-			        <p>The worm's neural activity is measured simultaneously with behavior.</p>
-			    </div>				
-			</div>
-			<!-- end Item -->
-			
-			<!-- Item -->
-			<div class="item">
-				<img src="assets/carousel/AVB.png" alt="" />
-				<div class="carousel-caption">
-			        <p>An interneuron's activity (AVB) is plotted with behavior.</p>
-			    </div>				
-			</div>
-			<!-- end Item -->
-			
-			
-			<!-- Item -->
-			<div class="item">
-				<img src="assets/carousel/CarlC_Icahn_Laboratory_Princeton_University_NewJersey_cc_carbonnyc.jpg" alt="" />
-				<div class="carousel-caption">
-			        <p>The Leifer Lab is located in the Carl C. Icahn Lab (credit: carbonnyc)</p>
-			    </div>			
-			</div>
-			<!-- end Item -->			
-
-			
-
-
-			
-		</div>
-		<!-- Carousel navigation -->
-		<a class="carousel-control left" href="#carousel" data-slide="prev"></a>
-		<a class="carousel-control right" href="#carousel" data-slide="next"></a>
+		</section> <!--End Row -->
 	</div>
-	<!-- end Macbook carousel -->
+							<hr />
+</section><!-- end .container -->
+						
+						
 
-</div>
-</div>
+							
+
+	<div class="container">
+		<div class="row">
+	
+			<div class="span4">
+											
+				<div class="media-grid pull-left leftImg">
+
+				      <img class="thumbnail" src="assets/leiferHead.jpg" alt="">
+
+			  </div>
+			  <h3>Andrew Leifer</h3><h4 class="grey"><i>Principal Investigator</i></h4>
+			  &nbsp;
+						<p> Located at the multi-disciplinary Lewis-Sigler Institute of integrative genomics the lab combines techniques from neuroscience, physics, engineering and computer science to study the dynamics of neural circuits in awake moving organisms.
+					</p>
+
+			</div>
+								
+			<div class="span8">
+								
+			<!-- Macbook carousel -->
+			<!-- Images should be 620 x 290 -->
+				<div id="carousel" class="carousel slide" rel="carousel">
+					<!-- Carousel items -->
+					<div class="carousel-inner">
+						<!-- Item -->
+						<div class="item active">
+							<a href="#"><img src="assets/carousel/blueAVM.jpg" alt="" /></a>
+							<div class="carousel-caption">
+								<p>To study neurocircuits, we build tools that monitor and manipulate neural activity in a worm with light.</p>
+							</div>
+						</div>
+						<!-- end Item -->
+			
+						<!-- Item -->
+						<!--<div class="item">
+							<img src="assets/carousel/colbertSchematic.png" alt="" />
+							<div class="carousel-caption">
+								<p>A high speed camera monitors the worm as it moves.</p>
+							</div>
+						</div>-->
+						<!-- end Item -->			
+			
+			
+						<!-- Item -->
+						<div class="item">
+							<img src="assets/carousel/computerVision.png" alt="" />
+							<div class="carousel-caption">
+								<p>Computer vision software identifies targets on the moving worm.</p>
+							</div>
+						</div> 
+						<!-- end Item -->
+			
+						<!-- Item -->
+						<div class="item">
+							<img src="assets/carousel/greenOptics.jpg" alt="" />
+							<div class="carousel-caption">
+								<p>Laser light is precisely targeted to neurons in the worm.</p>
+							</div>
+						</div>
+						<!-- end Item -->
+			
+			
+						<!-- Item -->
+						<div class="item">
+							<img src="assets/carousel/inhibitNerveCord.png" alt="" />
+							<div class="carousel-caption">
+								<p>Optogenetic proteins inhibit the worm's ventral nerve cord in response to green light. </p>
+							</div>
+						</div>
+						<!-- end Item -->
+
+										
+										
+					<!-- Item -->
+					<div class="item">
+						<img src="assets/carousel/escapeCartoon.png" alt="" />
+						<div class="carousel-caption">
+							<p>Changes to the worm's behavior are precisely quantified.</p>
+						</div>
+					</div>
+					<!-- end Item -->			
+			
+					<!-- Item -->
+					<div class="item">
+						<img src="assets/carousel/circuit.png" alt="" />
+						<div class="carousel-caption">
+							<p>These tools provide functional information about how neurons work together in a circuit.</p>
+						</div>
+					</div>
+					<!-- end Item -->			
+			
+
+			
+					<!-- Item -->
+					<div class="item">
+						<img src="assets/carousel/calcium_imaging.png" alt="" />
+						<div class="carousel-caption">
+							<p>The worm's neural activity is measured simultaneously with behavior.</p>
+						</div>				
+					</div>
+					<!-- end Item -->
+			
+					<!-- Item -->
+					<div class="item">
+						<img src="assets/carousel/AVB.png" alt="" />
+						<div class="carousel-caption">
+							<p>An interneuron's activity (AVB) is plotted with behavior.</p>
+						</div>				
+					</div>
+					<!-- end Item -->
+			
+			
+					<!-- Item -->
+					<div class="item">
+						<img src="assets/carousel/CarlC_Icahn_Laboratory_Princeton_University_NewJersey_cc_carbonnyc.jpg" alt="" />
+						<div class="carousel-caption">
+							<p>The Leifer Lab is located in the Carl C. Icahn Lab (credit: carbonnyc)</p>
+						</div>			
+					</div>
+					<!-- end Item -->			
+
+			
+
+
+			
+				</div> <!-- end carousel-inner-->
+				<!-- Carousel navigation -->
+					<a class="carousel-control left" href="#carousel" data-slide="prev"></a>
+					<a class="carousel-control right" href="#carousel" data-slide="next"></a>
+			</div>
+			<!-- end Macbook carousel -->
+		</div><!-- End span8-->
+
+	</div> <!-- end row -->
+							
+							
+							
+</div>  <!-- End container -->
+
+
+
+
+
+
 
 <section class="container">
-
-	<section class="row">
-		<article class="span12">
-			<h2><span class="grey">Optical</span> Neurophysiology of Behavior</h2>
-			<p>
-				Our lab seeks to understand how neural circuits encode complex behavior by combining techniques from neuroscience, physics, engineering and computer science. In particular, we study the model organism <i>C. elegans</i>, a microscopic worm with a simple nervous system capable of rich behaviors. We have pioneered optogenetic microscopy techniques  to optically manipulate and monitor neural activity in worms as they move.
-			</p>
-			<p><a class="" href="research.php">Read more &raquo;</a></p>
-		</article>
-
-	<!--	<article class="span4">
-			<h2><span class="grey">Lab</span> News</h2>
-			<p>New website is up and running!</p>
-			<p><a class="" href="#">Read more &raquo;</a></p>
-		</article> -->
-	</section>
-	<!-- end .row -->
-
 	<hr />
 
 	<section class="row" id="portfolio">
@@ -163,7 +186,7 @@
 					<!-- icon -->
 					<i class="icon-link"></i>
 					<!-- name -->
-					<b class="project-name">The Leifer Lab</b> will be joining<!--is a new member of--> the <a href="http://www.princeton.edu/neuroscience/" target="_blank">Princeton Neuroscience Institute</a>.
+					<b class="project-name">The Leifer Lab</b> is now part of the <a href="http://www.princeton.edu/neuroscience/" target="_blank">Princeton Neuroscience Institute</a>.
 				</div>
 				<!-- ribbon -->
 				<div class="ribbon"><span>New</span></div>
@@ -176,12 +199,12 @@
 				<a href="http://wyss.harvard.edu/viewpressrelease/96" title="Press release for DNA origami barcodes" target="_blank">
 					<!--298x186 -->
 					<img src="assets/barcode.png"></a>
-			<!-- 	<a href="#"></a> -->
-				<div class="project-description">
-					<b class="name">DNA Origami Nanobarcode</b> paper was recently  published in <a href="http://www.nature.com/nchem/journal/v4/n10/full/nchem.1451.html" target="_blank">Nature Chemistry</a> with Lin et al.
-					<i class="icon-file"></i>
-				</div>
-			</a>
+					<!-- 	<a href="#"></a> -->
+					<div class="project-description">
+						<b class="name">DNA Origami Nanobarcode</b> paper was recently  published in <a href="http://www.nature.com/nchem/journal/v4/n10/full/nchem.1451.html" target="_blank">Nature Chemistry</a> with Lin et al.
+						<i class="icon-file"></i>
+					</div>
+				</a>
 				
 			</div>
 		</article>
@@ -189,14 +212,14 @@
 		<article class="span4 project">
 			<div class="thumbnail equalizeHeight" >
 				<div class="vidclip"> 
-				<video id="sampleMovie" poster="assets/video/touch.png" controls>
+					<video id="sampleMovie" poster="assets/video/touch.png" controls>
 						<source src="assets/video/touch.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'/>
 						<source src="assets/video/touch.theora.ogv" type='video/ogg; codecs="theora, vorbis"' />
 						<source src="assets/video/touch.webm"  type='video/webm; codecs="vp8, vorbis"' />
 				        
 						<p><a href="https://vimeo.com/16933869" target="_blank">View this video on vimeo!</a></p> 
-				</video>
-			</div>
+					</video>
+				</div>
 				
 				<div class="project-description">
 					<b class="name">Blue light</b>
@@ -207,7 +230,8 @@
 
 	</section>	
 
-	</section>
+</section>
+
 
 	<?php 
 		include('php/footer.php');
--- php/footer.php
+++ php/footer.php
@@ -1,56 +1,56 @@
-	<!-- Footer
+<!-- Footer
 	================================================== 
 	end php/footer.php
-	-->
+-->
 	
 
-	<footer id="footer">
-		<div class="container">
-			<div class="row">
-				<div class="span4">
-					<a href="http://www.princeton.edu" title="Princeton University" target="_blank">
-						<img alt="Princeton University" src="assets/PUsig2-158U-ww-os.png">
-					</a>
-				</div>
-				<div class="span4 social-networks">
-					<h3>Find us elsewhere</h3>
-					<p> 
-						<a class="social-network icon-twitter" href="https://twitter.com/AndrewLeifer" target="_blank"></a>
-				<!-- <a class="social-network icon-facebook"></a> -->
+<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="span4">
+				<a href="http://www.princeton.edu" title="Princeton University" target="_blank">
+					<img alt="Princeton University" src="assets/PUsig2-158U-ww-os.png">
+				</a>
+			</div>
+			<div class="span4 social-networks">
+				<h3>Find us elsewhere</h3>
+				<p> 
+					<a class="social-network icon-twitter" href="https://twitter.com/AndrewLeifer" target="_blank"></a>
+					<!-- <a class="social-network icon-facebook"></a> -->
 					<!--	<a class="social-network icon-vimeo"></a> -->   
-						<a class="social-network icon-github" href="https://github.com/leiferlab" target="_blank"></a> 
-						<!-- <a class="social-network icon-linkedin"></a> --> 
-					</p>
-				</div>
-				<div class="span4 newsletter">
-					<a href="http://www.genomics.princeton.edu/" target="_blank">
-						<img src="assets/lewis_sigler_gray_transparent.png" alt="Lewis-Sigler Institute for Integrative Genomics">
-					</a>
+					<a class="social-network icon-github" href="https://github.com/leiferlab" target="_blank"></a> 
+					<!-- <a class="social-network icon-linkedin"></a> --> 
+				</p>
+			</div>
+			<div class="span4 newsletter">
+				<a href="http://www.genomics.princeton.edu/" target="_blank">
+					<img src="assets/lewis_sigler_gray_transparent.png" alt="Lewis-Sigler Institute for Integrative Genomics">
+				</a>
 
-				</div> 
-			</div> <!-- end .row -->		
-		</div> <!-- end .container -->
-	</footer><!-- end #footer -->
+			</div> 
+		</div> <!-- end .row -->		
+	</div> <!-- end .container -->
+</footer><!-- end #footer -->
 
-	<footer id="copyright">
-		<div class="container">
-			<div class="row">
-				<div class="span4">
-					&copy; 2012 <a href="http://www.princeton.edu" target="_blank">Princeton University</a>
-				</div>
-				<div class="span4">
+<footer id="copyright">
+	<div class="container">
+		<div class="row">
+			<div class="span4">
+				&copy; 2012 <a href="http://www.princeton.edu" target="_blank">Princeton University</a>
+			</div>
+			<div class="span4">
 					
-					<i class="icon-envelope"></i> <a href="mailto:leifer@princeton.edu" title="Send us email">leifer@princeton.edu</a> &nbsp; <i class="icon-phone"></i> (609) 258-2973 
-				</div>
-				<div class="span4">
-					<a href="http://www.genomics.princeton.edu/" target="_blank">Lewis-Sigler Institute for Integrative Genomics</a>
+				<i class="icon-envelope"></i> <a href="mailto:leifer@princeton.edu" title="Send us email">leifer@princeton.edu</a> &nbsp; <i class="icon-phone"></i> (609) 258-2973 
+			</div>
+			<div class="span4">
+				<a href="http://www.genomics.princeton.edu/" target="_blank">Lewis-Sigler Institute for Integrative Genomics</a>
 					
-				</div>
-			</div> <!-- end .row -->		
-		</div> <!-- end .container -->
-	</footer><!-- end #footer-extra -->
+			</div>
+		</div> <!-- end .row -->		
+	</div> <!-- end .container -->
+</footer><!-- end #footer-extra -->
 
-	<!-- Javascript - Placed at the end of the document so the pages load faster 
+<!-- Javascript - Placed at the end of the document so the pages load faster 
 	================================================== -->
 
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
@@ -62,3 +62,4 @@
 	
 </body>
 </html>		
+
