4839389f71e4f70917d7fac52ce9cd4efece9dce
Andrew Leifer <leifer@princeton.edu>
2013-01-29 10:14:21 -0500

    updated blurb

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 
 			  <h3>Andrew Leifer</h3><h4 class="grey"><i>Principal Investigator</i></h4>
 			  &nbsp;
-						<p> Located at the multi-disciplinary Lewis-Sigler Institute of integrative genomics the lab combines techniques from neuroscience, physics, engineering and computer science to study the dynamics of neural circuits in awake moving organisms.
+						<p>My research combines techniques from neuroscience, physics, engineering and computer science to study the dynamics of neural circuits in awake moving organisms. We are located at the inter-disciplinary Lewis-Sigler Institute for Integrative genomics.  
 					</p>
 
 			</div>
