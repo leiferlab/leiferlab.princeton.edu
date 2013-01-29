e9bec0125e718f1786c31d21da7a871e4b3c5986
Andrew Leifer <leifer@princeton.edu>
2013-01-29 09:27:55 -0500

    comma, removed google scholar

--- publications.php
+++ publications.php
@@ -8,8 +8,7 @@
 		<div class="hero-unit">	
 			<p> Publications</p>
 		</div>
-		<p><a href="http://scholar.google.com/citations?user=b0DQef8AAAAJ&hl=en">Google Scholar</a> Profile<br />
-			<a href="http://www.ncbi.nlm.nih.gov/pubmed?term=Andrew%20M%20Leifer[Author%20-%20Full]">PubMed</a>  Listing<br>
+		<p>	<a href="http://www.ncbi.nlm.nih.gov/pubmed?term=Andrew%20M%20Leifer[Author%20-%20Full]">PubMed</a>  Listing, 
 			ORCID: #<a href="http://orcid.org/0000-0002-5362-5093">0000-0002-5362-5093</a>
 		</p>
 	
@@ -19,7 +18,7 @@
 	<div class="textblock">
 		<h3>Peer-reviewed journal articles</h3>
 		<ul class="publications">
-			<li>Quan Wen, Michelle D. Po, Elizabeth Hulme, Sway Chen, Xinyu Liu, Sen Wai Kwok, Marc Gershow, <b>Andrew M. Leifer</b>, Victoria Butler, Christopher Fang-Yen, Taizo Kawano, William R. Schafer, George Whitesides, Mattieu Wyart, Dmitri Chklovskii and Aravinthan Samueal, "Proprioceptive coupling within motor neurons drives <i>C. elegans</i> forward locomotion." <a href="http://www.ncbi.nlm.nih.gov/pubmed/23177960"><i>Neuron</i></a>  76, 750-761 (2012). [<a href="http://gmwgroup.harvard.edu/pubs/pdf/1168.pdf">PDF</a>]</li>
+			<li>Quan Wen, Michelle D. Po, Elizabeth Hulme, Sway Chen, Xinyu Liu, Sen Wai Kwok, Marc Gershow, <b>Andrew M. Leifer</b>, Victoria Butler, Christopher Fang-Yen, Taizo Kawano, William R. Schafer, George Whitesides, Mattieu Wyart, Dmitri Chklovskii and Aravinthan Samueal, "Proprioceptive coupling within motor neurons drives <i>C. elegans</i> forward locomotion." <a href="http://www.ncbi.nlm.nih.gov/pubmed/23177960"><i>Neuron</i></a>,  76, 750-761 (2012). [<a href="http://gmwgroup.harvard.edu/pubs/pdf/1168.pdf">PDF</a>]</li>
 			<li>Chenxiang Lin, Ralf Jungmann, <b>Andrew M. Leifer</b>, Chao Li, Daniel Levner, Geroge M. Church, William M. Shih, Peng Yin. "Sub-micrometer geometrically encoded fluorescent barcodes self-assembled from DNA." <i><a href="http://www.nature.com/nchem/journal/v4/n10/full/nchem.1451.html">Nature Chemistry</a></i>, 4, 832–839 (2012).</li>
 			<li><b>Andrew M. Leifer</b>, Christopher Fang-Yen, Marc Gershow, Mark Alkema, Aravinthan D.T.  Samuel, "Optogenetic manipulation of neural activity in freely moving <i>Caenorhabditis elegans</i>," <a href="http://www.nature.com/nmeth/journal/v8/n2/full/nmeth.1554.html"><i>Nature Methods</i></a>, 8, 147–152 (2011). [<a href="http://colbert.physics.harvard.edu/media/leifer_et_al_2011.pdf">PDF</a>] </li>
 		<li>Kevin J. Coakley, David S. Simons, <b>Andrew M. Leifer</b>. "Secondary Ion Mass Spectrometry Measurements of Isotopic Ratios: Correction for Time Varying Count Rate." <a href="http://www.sciencedirect.com/science/article/pii/S138738060400421X"><a>International Journal of Mass Spectrometry</a></a>, 204 (2005) 107-120.
