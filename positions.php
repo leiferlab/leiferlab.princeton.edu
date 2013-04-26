9ffb2fe5578f76515eeb903b62aef9b1db1fdc58
Andrew Leifer <andrew.leifer@gmail.com>
2013-04-25 21:38:43 -0400

    killed post-bac posting

--- positions.php
+++ positions.php
@@ -20,8 +20,6 @@
 <h3>Undergraduates</h3>
 <p>Princeton undergraduates interested in conducting research in the summer, during the school year or as part of a Junior or Senior Thesis should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. <!--Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st. --></p>
 
-<h3>Post-Bacs</h3>
-<p>Send a CV and references to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a>.</p>
 
 </div>
 	</section>
