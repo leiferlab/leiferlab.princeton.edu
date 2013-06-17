5349808ceccdd61a9e24387d822ab9d40cc98c7a
Andrew Leifer <leifer@princeton.edu>
2013-06-17 17:05:12 -0400

    added photos of new lab members

--- people.php
+++ people.php
@@ -30,7 +30,55 @@
 				<img alt="" src="assets/team/andyHeadSq.jpg" />
 			</div>
 		</article>
-
+		
+		
+		<article class="member">
+			<h3 class="member-name">Ashley Linder</h3>
+			<h4 class="member-position">Graduate Student</h4>
+			<p class="member-contact">
+				<i class="icon-envelope"></i>&nbsp;<a href="mailto:linder@princeton.edu">linder@princeton.edu</a><br \>
+				<i class="icon-phone"></i> (609) 258-2973<br \>
+			</p>
+			<p class="member-bio">
+				Ashley Linder is a Graduate Student in the physics department. She grew up in California, and abandoned a promising career as a junior competitive surfer during a near drowning experience. Her fear of the ocean prompted her to stay indoors to study mathematics (which she majored in at UCSD) and, eventually, propelled her to Princeton, as far away from the Pacific Ocean as she could get. Ashley has resumed surfing and sold her life story rights to make the movie Blue Crush, starring Kate Bosworth.
+			</p>
+			<div class="member-avatar">
+				<img alt="" src="assets/team/LinderHeadSq.jpg" />
+			</div>
+		</article>
+		
+		<article class="member">
+			<h3 class="member-name">Fred Shipley</h3>
+			<h4 class="member-position">Research Specialist</h4>
+			<p class="member-contact">
+				Fred Shipley is a full time Research Assistant.  He grew up in Brooklyn, where a botched blood infusion as a child resulted in him being able to sense electromagnetic fields in an environment.  This engendered an interest in biophysics, which he majored in at Amherst while working in the lab of Ashley Carter.  When not in lab, Fred enjoys competitive hot dog eating and developing his abilities as a snake charmer.				
+				<i class="icon-envelope"></i>&nbsp;<a href="mailto:fshipley@princeton.edu">fshipley@princeton.edu</a><br \>
+				<i class="icon-phone"></i> (609) 258-2973<br \>
+			</p>
+			<p class="member-bio">
+				
+			</p>
+			<div class="member-avatar">
+				<img alt="" src="assets/team/shipleyHeadSq.jpg" />
+			</div>
+		</article>
+		
+		<article class="member">
+			<h3 class="member-name">Kevin Mizes</h3>
+			<h4 class="member-position">Undergraduate Student</h4>
+			<p class="member-contact">
+								
+				<i class="icon-envelope"></i>&nbsp;<a href="mailto:linder@princeton.edu">linder@princeton.edu</a><br \>
+				<i class="icon-phone"></i> (609) 258-2973<br \>
+			</p>
+			<p class="member-bio">
+				Kevin Mizes is an undergraduate at Princeton University pursuing a degree in Physics. He plans for a future career in research. Kevin grew up in cloudy Rochester, New York.
+			</p>
+			<div class="member-avatar">
+				<img alt="" src="assets/team/mizesHeadSq.jpg" />
+			</div>
+		</article>
+		
 		<article class="member">
 			<h3 class="member-name">You</h3>
 			<h4 class="member-position">Scientist</h4>
