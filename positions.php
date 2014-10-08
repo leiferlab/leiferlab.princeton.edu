a0a9fd605285d2bc596a5d6619d8defabbc550ea
Andrew Leifer <leifer@princeton.edu>
2014-10-08 12:09:43 -0400

    further clarified no undergrads during school year

--- positions.php
+++ positions.php
@@ -18,7 +18,7 @@
 <p>Current or incoming Princeton graduate students interested in a rotation should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting.</p>
 
 <h3>Undergraduates</h3>
-<p>Princeton undergraduates interested in conducting research in the summer or during the year as part of a Junior or Senior Thesis should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. <!--Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st. --></p>
+<p>Princeton undergraduates interested in conducting research in the summer or as part of a Junior or Senior Thesis should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. <!--Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st. --></p>
 
 
 </div>
