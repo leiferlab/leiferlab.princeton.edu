33a10930b3ba5cbab76057162cd29c61d2cdd306
Andrew Leifer <leifer@princeton.edu>
2015-05-26 15:53:00 -0400

    removed job posting

--- positions.php
+++ positions.php
@@ -8,64 +8,15 @@
 
 
 	<div class="hero-unit">
-		<p>We're looking for talented researchers.</p> 
+	<!--	<p>We're looking for talented researchers.</p> -->
+	<p>We'll get back to you on that...</p>
 	</div>
 	<div class="meaty">
 	<!--<p>Our work spans many disciplines. If you have a background in molecular biology, neuroscience, physics, engineering or programming and are interested in applying your skills towards learning how neural circuits drive behavior, then please contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> by email.</p>-->
 	<hr />
-<h3>Research Specialist</h3>
+<h3>There are currently no ongoing searches</h3>
 
-<p>The Lewis-Sigler Institute for Integrative Genomics seeks applicants for a research specialist to join the laboratory of Andrew Leifer whose research interest focuses on developing novel imaging technology to better understand neural function using nematodes as a model system.</p>
-
-<p>This will be a one-year term position.</p>
-
-<p>Specific duties include:
-<ul>
-
-<li>Maintaining C. elegans stocks for experimentation.</li>
-<li>Generating C. elegans transgenic strains in-house and performing molecular cloning</li>
-<li>Coordinating the outsourcing of cloning, sequencing or creation of transgenic strains</li>
-<li>Preparation, organization and purchasing of equipment, supplies and reagents and other support duties to facilitate a safe, productive and efficient lab environment.</li>
-<li>Conducting laboratory experiments independently or in support of others and carefully managing and recording experimental conditions.</li>
-<li>Recording and analyzing fluorescent microscopy images and videos.</li>
-<li>Basic data analysis and problem solving.</li>
-<li>Familiarize her or himself with the current literature and projects of the lab.</li>
-<li>Maintaining a longer-term research project and participating in group meetings.</li>
-<li>Perform other related duties as assigned.</li>
-</ul>
-
-
-Essential Qualifications
-<ul>
-<li>BA or BS in biology, neuroscience, bioengineering or related field.</li>
-<li>Working knowledge of genetics, molecular cloning and PCR</li>
-<li>Ability to conduct laboratory experiments independently and carefully manage and record experimental conditions.</li>
-<li>Ability to maintain a meticulous daily laboratory notebook</li>
-<li>Ability to work well with others</li>
-<li>Ability to seek out information from the scientific literature</li>
-<li>Excellent computer skills with proficiency in Microsoft Word, Excel, PowerPoint, and some experience using MATLAB or Python or another comparable programming or scientific computing environment.</li>
-<li>Applicant must have excellent organizational skills and flexibility to handle multiple tasks</li> simultaneously. The applicant should have a strong attention to detail and an independent problem-solving mentality.</li>
-</ul>
-</p>
-
-
-<p>Preferred Qualifications:
-	<ul>
-<li>Prior experience working with nematodes or other model organisms is preferred</li>
-<li>Prior experience performing molecular cloning and/or familiarity with restriction enzymes, Gateway or Gibson cloning techniques is preferred</li>
-<li>Familiarity with quantitative approaches to biology or neuroscience preferred.</li>
-<li>The ideal candidate is one who seeks professional development in scientific research or a medical profession.</li>
-<li>Experience with fluorescent microscopy preferred.</li>
-<li>Willingness to conduct independent research and participate in project planning and experimental design.</li>
-</p>
-<p>Please apply at <a href="https://jobs.princeton.edu/applicants/jsp/shared/position/JobDetails_css.jsp?postingId=210188">http://jobs.princeton.edu</a> Requisition #1500191.
-</p>
-
-
-
-
-
-
+<p>Future searches and openings will be posted here.</p>
 
 <!-- ><h3>Graduate Students</h3>
 <p>Current or incoming Princeton graduate students interested in a rotation should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting.</p>
