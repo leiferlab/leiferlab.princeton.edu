f3a98da34c6bac0b686bdaa9f374207a2fc77bc4
Andrew Leifer <leifer@princeton.edu>
2015-11-11 13:27:53 -0500

    fix typo in publications

--- publications.php
+++ publications.php
@@ -19,7 +19,7 @@
 		<h3>Peer-reviewed journal articles</h3>
 		<ul class="publications">
 
-<li><Jeffrey P. Nguyen, Frederick B. Shipley, Ashley N. Linder, George S. Plummer, Mochi Liu, Sagar U. Setru, Joshua W. Shaevitz, <b>Andrew M. Leifer</b>. "Whole-brain calcium imaging with cellular resolution in freely behaving <i>Caenorhabditis elegans</i>" P.N.A.S., in press. Pre-peer-review manuscrit available on <a href="http://arxiv.org/abs/1501.03463">arXiv</a> [<a href="http://arxiv.org/pdf/1501.03463v1">PDF</a>]. </li>
+<li>Jeffrey P. Nguyen, Frederick B. Shipley, Ashley N. Linder, George S. Plummer, Mochi Liu, Sagar U. Setru, Joshua W. Shaevitz, <b>Andrew M. Leifer</b>. "Whole-brain calcium imaging with cellular resolution in freely behaving <i>Caenorhabditis elegans</i>" P.N.A.S., in press. Pre-peer-review manuscrit available on <a href="http://arxiv.org/abs/1501.03463">arXiv</a> [<a href="http://arxiv.org/pdf/1501.03463v1">PDF</a>]. </li>
 			<li>Frederick B. Shipley,  Christopher M. Clark,  Mark J. Alkema, <b>Andrew M. Leifer</b>. "Simultaneous optogenetic manipulation and calcium imaging in freely moving <i>C. elegans</i>."  <i><a href="http://journal.frontiersin.org/Journal/10.3389/fncir.2014.00028/">Frontiers in Neural Circuits</a></i> 8:28. (2014). [<a href="http://journal.frontiersin.org/Journal/81403/pdf">PDF</a> and <a href="http://vimeo.com/87910242">Video</a>]</li>
 			
 			<li>Steven J. Husson, Alexander Gottschalk, <b>Andrew M. Leifer</b>. "Optogenetic manipulation of neural activity in <i>C. elegans</i>: from synapse to circuits and behavior." <i><a href="http://onlinelibrary.wiley.com/doi/10.1111/boc.201200069/abstract">Biology of the Cell</a></i> (2013). [<a href="http://arxiv.org/pdf/1303.2876v1">PDF</a> via <a href="http://arxiv.org/abs/1303.2876">arXiv</a>]</li>
@@ -30,10 +30,6 @@
 		<li>Kevin J. Coakley, David S. Simons, <b>Andrew M. Leifer</b>. "Secondary Ion Mass Spectrometry Measurements of Isotopic Ratios: Correction for Time Varying Count Rate." <a href="http://www.sciencedirect.com/science/article/pii/S138738060400421X"><a><i>International Journal of Mass Spectrometry</i></a></a>, 204 (2005) 107-120.
 		</li>
 		</ul>		
-	<h3>Manuscripts submitted for peer-review</h3>
-	<ul class="publications">
-			<li>Jeffrey P. Nguyen,  Frederick B. Shipley,  Ashley N. Linder, George S. Plummer, Joshua W. Shaevitz,  <b>Andrew M. Leifer</b>. "Whole-brain calcium imaging with cellular resolution in freely behaving <i>C. elegans</i>." Manuscript available on <a href="http://arxiv.org/abs/1501.03463">arXiv</a> [<a href="http://arxiv.org/pdf/1501.03463v1">PDF</a>]</li>
-	</ul>  
 			
 	</div>
 
