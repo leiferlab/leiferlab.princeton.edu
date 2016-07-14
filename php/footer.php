9b832759544c82427929612b237f0e5f1ac807f4
Andrew Leifer <leifer@princeton.edu>
2016-07-14 10:07:21 -0400

    more playing with icons in copyright bar

--- php/footer.php
+++ php/footer.php
@@ -51,6 +51,7 @@
 			</div>
 			
 			<p class="span2">
+				<a href="mailto:leifer@princeton.edu" class="social-network icon-envelope" target="_blank" title="Email leifer@princeton.edu"></a> &nbsp;
 				<a href="https://twitter.com/AndrewLeifer" class="social-network icon-twitter" target="_blank" title="Twitter"></a> &nbsp;
 				<a href="https://github.com/leiferlab/" class="social-network icon-github" target="_blank" title="Github"></a> &nbsp;
 				<a href="https://vimeo.com/user11723595" class="social-network icon-vimeo" target="_blank" title="Vimeo"></a> &nbsp;
