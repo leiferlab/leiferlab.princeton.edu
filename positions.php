e7a32db9b97925558f64d378b7336ce13358f739
Andrew Leifer <leifer@princeton.edu>
2016-07-14 14:44:15 -0400

    listed four positions

--- positions.php
+++ positions.php
@@ -8,23 +8,200 @@
 
 
 	<div class="hero-unit">
-	<!--	<p>We're looking for talented researchers.</p> -->
-	<p>We'll get back to you on that...</p>
+		<p>We're looking for talented researchers.</p> 
+	<!-- <p>We'll get back to you on that...</p> -->
 	</div>
 	<div class="meaty">
-	<!--<p>Our work spans many disciplines. If you have a background in molecular biology, neuroscience, physics, engineering or programming and are interested in applying your skills towards learning how neural circuits drive behavior, then please contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> by email.</p>-->
+	<p>Our work spans disciplines. We seek individuals with backgrounds in physics, neuroscience, molecular biology, engineering or programming who are interested in applying their skills towards investigating how patterns of neural activity drive behavior.</p>
 	<hr />
-<h3>There are currently no ongoing searches</h3>
+<!-- <h3>There are currently no ongoing searches</h3> -->
 
-<p>Future searches and openings will be posted here.</p>
+<!-- <p>Future searches and openings will be posted here.</p> -->
 
-<!-- ><h3>Graduate Students</h3>
-<p>Current or incoming Princeton graduate students interested in a rotation should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting.</p>
+<h3>Staff scientist</h3>
+<p>A staff scientist is sought to perform and manage nematode transgenic efforts in the Leifer Lab. This appointment will be made at the rank of Associate Professional Specialist or more senior.  The Leifer Lab studies the patterns of neural activity that drive behavior in the nematode C. elegans using novel imaging approaches that combine techniques from physics, neuroscience and molecular biology. The Leifer Lab is affiliated with Princeton University’s Department of Physics and the Princeton Neuroscience Institute.</p>
+
+<p>Specific duties include:
+<ul>
+<li>Designing and generating novel transgenic strains of the nematode C. elegans optimized for calcium imaging and optogenetic applications. </li>
+<li>Researching and testing new gene editing techniques and adapting them for use in the lab.</li>
+<li>Carrying out independent investigations that focus on transgenics and molecular genetics.</li>
+<li>Training incoming lab members in safe nematode handling, maintenance and transgenics.</li>
+<li>Mentoring undergraduate students and serving as a resource for graduate students.</li>
+<li>Developing and maintaining protocols for working with C. elegans </li>
+<li>Maintaining C. elegans stocks for experimentation.</li>
+<li>Maintaining and purchasing equipment, supplies and reagents and other support duties to facilitate a safe, productive and efficient lab environments.</li>
+<li>Setting safety standards in the lab and developing newer and safer ways of performing existing assays.</li>
+<li>Familiarize themselves with the current literature and projects of the lab.</li>
+<li>Present work in regular group meetings.</li>
+<li>Perform other related duties as assigned.</li>
+</ul>
+</p>
+
+<p>Essential qualifications:
+<ul>
+	<li>Advanced degree in molecular or cellular biology, neuroscience, bioengineering or related field.</li>
+	<li>Prior experience working independently on complex multipart scientific investigation.</li>
+	<li>Prior experience performing nematode transgenics, including plasmid design, molecular cloning PCR, and microinjection.</li>
+	<li>Prior experience with fluorescent microscopy.</li>
+	<li>Prior exposure to neurobiology.</li>
+	<li>Ability to conduct laboratory experiments independently and carefully manage and record experimental conditions.  </li>
+	<li>Ability to maintain a meticulous daily laboratory notebook. </li>
+	<li>Ability to work well with others.</li>
+	<li>Ability to seek out new knowledge and techniques from the scientific literature.</li>
+	<li>Excellent computer skills with proficiency in Microsoft Word, Excel, PowerPoint, </li>
+	<li>Applicant must have excellent organizational skills and flexibility to handle multiple tasks simultaneously.  The applicant should have a strong attention to detail and an independent problem-solving mentality.</li>
+</ul>
+</p>
+
+<p>Preferred Qualifications:
+<ul>
+	<li>Prior experience with Gateway or Gibson cloning techniques is preferred.</li>
+	<li>Prior experience with CRISPR or other advanced gene editing techniques is preferred.</li>
+	<li>Familiarity with quantitative approaches to biology or neuroscience preferred.</li>
+	<li>Prior exposure to MATLAB or Python or another comparable programming language is preferred.</li>
+	<li>Prior experience mentoring undergraduates is preferred. </li>
+</ul>
+</p>
+<p>Princeton University is an equal opportunity employer and all qualified applicants will receive consideration for employment without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, disability status, protected veteran status, or any other characteristic protected by law.  This position is subject to the University’s background check policy.</p>
+
+
+
+
+
+<h3>Post-docs</h3>
+<h2>Postdoctoral Research Associate: Whole Brain Imaging</h2>
+<p> A postdoctoral research associate or more senior is sought for a whole-brain neural imaging project in moving animals in the Leifer Laboratory at Princeton University's Department of Physics, affiliated with the Princeton Neuroscience Institute. The Leifer Lab develops novel optical techniques for probing neural dynamics that drive behavior in the nematode <i>C. elegans</i>.</p>
+<p> A PhD in a related field is required as well as a background in experimental physics, optics, computer science, electrical engineering or neuroscience with expertise building and using custom scientific instruments for precision measurements. In particular, someone is sought who is comfortable mixing custom optical and electronics hardware and custom software. This position is subject to the University's background check policy. </p>
+
+<p>Ideal attributes are: 
+<ul>
+	<li>Talented experimentalist with a history of performing difficult measurements</li>
+	<li>Talented troubleshooter of hardware and software</li>
+	<li>Experience with fluorescence microscopy and fundamentals of microscope design</li>
+	<li>Fluency in at least one programming language, and prior exposure to Matlab, LabView and C.</li>
+	<li>Able and willing to dive into large code-bases</li>
+	<li>Experience with digital signal processing and image processing</li>
+	<li>Comfortable learning new topics independently via the literature in the areas of computer vision, machine learning, optics or electronics</li>
+	<li>Interested in neuroscience and/or life sciences, although prior experience in these areas is not strictly necessary</li>
+	<li>Willing to balance competing demands for rapid instrument design and troubleshooting with the need to design tools that are sustainable, maintainable and user-friendly</li>
+	<li>Ability to mentor others</li>
+	<li>Someone who is organized, self-motivated</li>
+	<li>Able to work well in a team, but also able to work independently</li>
+	<li>Clear communicator</li>
+</ul>
+</p> 
+<p>Please submit CV, cover letter and references through Princeton University's <a href="http://www.princeton.edu/physics/about/employment/">Department of Physics Employment page.</a></p>
+<p>Princeton University is an equal opportunity employer and all qualified applicants will receive consideration for employment without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, disability status, protected veteran status, or any other characteristic protected by law.</p>
+
+
+<h2>Postdoctoral Research Associate: Whole Brain Imaging</h2>
+<p> A postdoctoral research associate or more senior is sought for a whole-brain neural imaging project in moving animals in the Leifer Laboratory at Princeton University's Department of Physics, affiliated with the Princeton Neuroscience Institute. The Leifer Lab develops novel optical techniques for probing neural dynamics that drive behavior in the nematode <i>C. elegans</i>.</p>
+<p> A PhD in a related field is required as well as a background in experimental physics, optics, computer science, electrical engineering or neuroscience with expertise building and using custom scientific instruments for precision measurements. In particular, someone is sought who is comfortable mixing custom optical and electronics hardware and custom software. This position is subject to the University's background check policy. </p>
+
+<p>Ideal attributes are: 
+<ul>
+	<li>Talented experimentalist with a history of performing difficult measurements</li>
+	<li>Talented troubleshooter of hardware and software</li>
+	<li>Experience with fluorescence microscopy and fundamentals of microscope design</li>
+	<li>Fluency in at least one programming language, and prior exposure to Matlab, LabView and C.</li>
+	<li>Able and willing to dive into large code-bases</li>
+	<li>Experience with digital signal processing and image processing</li>
+	<li>Comfortable learning new topics independently via the literature in the areas of computer vision, machine learning, optics or electronics</li>
+	<li>Interested in neuroscience and/or life sciences, although prior experience in these areas is not strictly necessary</li>
+	<li>Willing to balance competing demands for rapid instrument design and troubleshooting with the need to design tools that are sustainable, maintainable and user-friendly</li>
+	<li>Ability to mentor others</li>
+	<li>Someone who is organized, self-motivated</li>
+	<li>Able to work well in a team, but also able to work independently</li>
+	<li>Clear communicator</li>
+</ul>
+</p> 
+<p>Please submit CV, cover letter and references through Princeton University's <a href="http://www.princeton.edu/physics/about/employment/">Department of Physics Employment page.</a></p>
+<p>Princeton University is an equal opportunity employer and all qualified applicants will receive consideration for employment without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, disability status, protected veteran status, or any other characteristic protected by law.</p>
+
+
+
+<h2>Postdoctoral Research Associate: Systems Neuroscience</h2>
+<p>A postdoctoral researcher or more senior is sought for a systems neuroscience project in the Leifer Lab investigating how a nervous system's structure informs or constrains neural function in the nematode C. elegans. The project will involve sophisticated transgenics and optical imaging. The Leifer Lab is affiliated with Princeton University's Department of Physics and the Princeton Neuroscience Institute. This position is subject to the University's background check policy.</p>
+<p> A Ph.D. in molecular biology, neuroscience, genetics or related field is required. Quantitative and mathematical fluency will also be critical. In particularly, someone is sought who is comfortable applying cutting edge genetic techniques in a neuroscience context as part of a large multipart quantitative scientific investigation.</p>
+
+<p>Ideal attributes are:
+	<ul>
+	<li>Talented at the bench with a history of performing difficult molecular or genetic manipulations</li>
+	<li>Talented troubleshooter of complicated molecular biology protocols</li>
+	<li>Prior exposure to neuroscience and/or C. elegans research</li>
+	<li>Experience carrying complex multi-part scientific investigations through to completion and publication</li>
+	<li>Experience performing fluorescence microscopy and working with genetically encoded fluorescent proteins</li>
+	<li>Comfortable learning new topics independently via the literature in the areas of gene-editing, C. elegans neuroscience, optical indicators</li>
+	<li>Comfortable using mathematical concepts from linear algebra and statistics</li>
+	<li>Comfortable coding in at least one programming language, and prior exposure to Matlab.</li>
+	<li>Creative thinker</li>
+	<li>Ability to mentor others</li>
+	<li>Someone who is exquisitely organized, detailed-oriented and self-motivated</li>
+	<li>Excellent time management skills</li>
+	<li>Able to work well in a team, but also able to work independently</li>
+	<li>Clear communicator    </li>
+	<ul>
+</p>
+<p>Please submit CV, cover letter and references through Princeton University's <a href="http://www.princeton.edu/physics/about/employment/">Department of Physics Employment page.</a></p>
+<p>Princeton University is an equal opportunity employer and all qualified applicants will receive consideration for employment without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, disability status, protected veteran status, or any other characteristic protected by law.</p>
+	  
+<h3>Research Specialist</h3>
+<p>A research specialist is sought to perform nematode transgenics in support of research efforts in the Leifer Lab. The Leifer Lab studies patterns of neural activity that drive behavior in the nematode C. elegans using novel imaging approaches that combine techniques from physics, neuroscience and molecular biology. The Leifer Lab is affiliated with Princeton University’s Department of Physics and the Princeton Neuroscience Institute.</p>
+
+<p>Specific duties include:
+<ul>
+	<li>Maintaining C. elegans stocks for experimentation.</li>
+	<li>Performing molecular cloning and plasmid design</li>
+	<li>Microinjecting C. elegans for creating new transgenic nematode  strains</li>
+	<li>Preparing, organizing and purchasing equipment, supplies and reagents </li>
+	<li>Other support duties to facilitate a safe, productive and efficient lab environment.</li>
+	<li>Conducting laboratory experiments independently or in support of others and carefully managing and recording experimental conditions.</li>
+	<li>Recording and analyzing fluorescent microscopy images and videos.</li>
+	<li>Basic data analysis and problem solving.</li>
+	<li>Familiarize her or himself with the current literature and projects of the lab.</li>
+	<li>Perform other related duties as assigned.</li>
+</ul>
+</p>
+<p>
+Essential qualifications:
+<ul>
+	<li>BA or BS in biology, neuroscience, bioengineering or related field. </li>
+	<li>Prior experience with genetics, molecular cloning and PCR</li>
+	<li>Prior experience working with a genetic model systems</li>
+	<li>Ability to conduct laboratory experiments independently and carefully manage and record experimental conditions.  </li>
+	<li>Ability to maintain a meticulous daily laboratory notebook </li>
+	<li>Ability to work well with others</li>
+	<li>Ability to seek out information from the scientific literature</li>
+	<li>Excellent computer skills with proficiency in Microsoft Word, Excel and PowerPoint.</li>
+	<li>Applicant must have excellent organizational skills and flexibility to handle multiple tasks simultaneously.  The applicant should have a strong attention to detail and an independent problem-solving mentality.</li>
+</ul>
+</p>
+<p>
+Preferred qualifications:
+<ul>
+<li>Prior experience working with C. elegans is preferred</li>
+<li>Prior experience with microinjection is preferred</li>
+<li>Prior experience with Gateway or Gibson cloning techniques is preferred</li>
+<li>Prior experience with CRISPR or other advanced gene editing techniques is preferred</li>
+<li>Prior experience with fluorescent microscopy is preferred.</li>
+<li>Experience using MATLAB or Python or another programming or scientific computing environment is preferred</li>
+<li>Familiarity with quantitative approaches to biology or neuroscience preferred. </li>
+<li>Willingness to conduct independent research and participate in project planning and experimental design.</li>
+</ul>
+
+<p><i>Formal job posting to follow. For now please contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> directly.</i></p>
+
+
+
+
+
+<h3>Graduate Students</h3
+<p>Current or incoming Princeton graduate students interested in rotating or joining the lab should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting. Dr. Leifer participates in the <a href="https://www.princeton.edu/physics/graduate-program/">Physics</a>, <a href="https://pni.princeton.edu/graduate-program/ph.d.-neuroscience">Neuroscience</a> and <a href="http://lsi.princeton.edu/qcbgraduate/">Quantitative and Computational Biology</a> graduate programs. Prospective graduate students  should also explore Princeton's flexible umbrella lifesciences graduate progra, <a href="https://www.princeton.edu/biology/">Biology@Princeton</a>.</p>
 
 <h3>Undergraduates</h3>
-<p>Princeton undergraduates interested in conducting research in the summer or as part of a Junior or Senior Thesis should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. 
--->
-	<!--Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st. --></p>
+<p>Princeton undergraduates interested in conducting research in the summer or as part of a Junior or Senior Thesis should contact <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st.</p>
+
 
 
 </div>
