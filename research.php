eb507cd054f985c37f8fd77e33ebf2c9f1c7c9b4
Andrew Leifer <leifer@princeton.edu>
2016-07-14 10:45:36 -0400

    updated research text

--- research.php
+++ research.php
@@ -9,15 +9,15 @@ How do a collection of neurons work together to receive information from the env
 	
 	
 	<h3>Probing neural circuits</h3>	
-	<p>To probe the neural circuits underlying behavior, it is important to perturb and observe the activity of individual neurons and their effect on whole-organism behavior. The Leifer Lab specializes in developing  non-invasive techniques to stimulate and record from ensembles of neurons across an organism with single-cell resolution in awake, intact, unrestrained animals whose behavior can be observed simultaneously. We build upon recent advances in the emerging field of optogenetics to use light to stimulate or inhibit neurons in a worm as it moves. We also use genetically encoded calcium indicators to observe neural activity in the worm as it moves. And we use computer vision software to quantify the worm's motion. </p>
+	<p>To probe the neural circuits underlying behavior, it is important to perturb and observe the activity of individual neurons and their effect on whole-organism behavior. The Leifer Lab specializes in developing  non-invasive techniques to stimulate and record from ensembles of neurons across an organism with single-cell resolution in awake, intact, unrestrained animals whose behavior can be observed simultaneously. We build upon recent advances in the emerging field of optogenetics to use light to stimulate or inhibit neurons in a worm as it moves. We also use genetically encoded calcium indicators to record neural activity from all the neurons in the brain of the worm as it moves. And we use computer vision software to quantify and study the worm's behavior. </p>
 
-	<p>By systematically studying the contribution of individual neurons to behavior, we hope to uncover general principles about the functions of neural circuits. 
+	<p>By systematically studying the collective neural dynamics that drive behavior, we hope to uncover general principles about the functions of neural circuits. 
 
 	
 
 <h3>Why C. Elegans?</h3>
 <p>Invertebrate model organisms can reveal fundamental principles of neuroscience. Insights into the molecular basis of learning and memory, for example, came  initially from studies of <i>Aplysia</i>, a giant sea slug. Similarly, our understanding of motor circuits came first from early studies of leech and lamprey.<p> 
-	<p>The nematode <i>Caenorhabditis elegans</i> is a powerful tool for studying the neuronal basis of behavior. With only 302 neurons, its nervous system is compact but tractable and the 1 mm-long nematode exhibits a rich array of behaviors. It senses its environment, navigates toward food and temperatures  that it prefers, avoids chemicals that it dislikes, and responds to touch. The worm can even learn to  associate odors with foods that make it sick, and avoid them accordingly, for example.</p>
+	<p>The nematode <i>Caenorhabditis elegans</i> is a powerful tool for studying the neuronal basis of behavior. With only 302 neurons, its nervous system is compact but tractable and the 1 mm-long nematode exhibits a rich array of behaviors. It senses its environment, navigates toward food and temperatures  that it prefers, avoids chemicals that it dislikes, and responds to touch. The worm can even learn to  associate neutral odors with food and then will seek out that odor accordingly, for example.</p>
 	<p><i>C. elegans</i> is especially well suited to optical neurophysiology studies. The entire wiring diagram of the <i>C. elegans</i>  nervous system (its connectome) has been mapped completely. The worm is easy to manipulate genetically, and its optically transparent body provides easy access for optical tools. 
 		
 		<h3>Video</h3>
