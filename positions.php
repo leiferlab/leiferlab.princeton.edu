230bf0eb0c34bf35678c90e0eb060c1413370cbc
Andrew Leifer <leifer@princeton.edu>
2017-02-23 09:18:59 -0500

    added back in sysneuro position

--- positions.php
+++ positions.php
@@ -39,6 +39,8 @@
 
 
 <h2><a name="postdoc"></a>Post-docs</h2>
+Those interested in postdoctoral positions should feel free to contact Andrew Leifer directly at <a href="mailto:leifer@princeton.edu">leifer@princeton.edu</a>.
+
 <h3>Postdoctoral Research Associate: Whole Brain Imaging - #1600539</h3>
 <p> A postdoctoral research associate or more senior is sought for a whole-brain neural imaging project in moving animals in the Leifer Laboratory at Princeton University's Department of Physics, affiliated with the Princeton Neuroscience Institute. The Leifer Lab develops novel optical techniques for probing neural dynamics that drive behavior in the nematode <i>C. elegans</i>.</p>
 <p> A PhD in a related field is required as well as a background in experimental physics, optics, computer science, electrical engineering or neuroscience with expertise building and using custom scientific instruments for precision measurements. In particular, someone is sought who is comfortable mixing custom optical and electronics hardware and custom software. This position is subject to the University's background check policy. </p>
@@ -64,6 +66,32 @@
 <p>Princeton University is an equal opportunity employer and all qualified applicants will receive consideration for employment without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, disability status, protected veteran status, or any other characteristic protected by law.</p>
 
 
+<h3>Postdoctoral Research Associate: Systems Neuroscience - #1600540</h3>
+<p>A postdoctoral researcher or more senior is sought for a systems neuroscience project in the Leifer Lab investigating how a nervous system's structure informs or constrains neural function in the nematode C. elegans. The project will involve sophisticated transgenics and optical imaging. The Leifer Lab is affiliated with Princeton University's Department of Physics and the Princeton Neuroscience Institute. This position is subject to the University's background check policy.</p>
+<p> A Ph.D. in molecular biology, neuroscience, genetics or related field is required. Quantitative and mathematical fluency will also be critical. In particularly, someone is sought who is comfortable applying cutting edge genetic techniques in a neuroscience context as part of a large multipart quantitative scientific investigation.</p>
+
+<p>
+	<ul><i>Ideal attributes are: </i>
+	<li>Talented at the bench with a history of performing difficult molecular or genetic manipulations</li>
+	<li>Talented troubleshooter of complicated molecular biology protocols</li>
+	<li>Prior exposure to neuroscience and/or C. elegans research</li>
+	<li>Experience carrying complex multi-part scientific investigations through to completion and publication</li>
+	<li>Experience performing fluorescence microscopy and working with genetically encoded fluorescent proteins</li>
+	<li>Comfortable learning new topics independently via the literature in the areas of gene-editing, C. elegans neuroscience, optical indicators</li>
+	<li>Comfortable using mathematical concepts from linear algebra and statistics</li>
+	<li>Comfortable coding in at least one programming language, and prior exposure to Matlab.</li>
+	<li>Creative thinker</li>
+	<li>Ability to mentor others</li>
+	<li>Someone who is exquisitely organized, detailed-oriented and self-motivated</li>
+	<li>Excellent time management skills</li>
+	<li>Able to work well in a team, but also able to work independently</li>
+	<li>Clear communicator    </li>
+	</ul>
+</p>
+<p>To apply, please submit CV, cover letter and list of three references through the <a href="https://jobs.princeton.edu">Jobs at Princeton website</a>. Search for open position <b>requisition number 1600540</b></a>.</p>
+
+
+<p>Princeton University is an equal opportunity employer and all qualified applicants will receive consideration for employment without regard to race, color, religion, sex, sexual orientation, gender identity, national origin, disability status, protected veteran status, or any other characteristic protected by law.</p>
 
 
 </div>
