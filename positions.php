7b48a64533d87b6ac10df71c1a97d261a6761f0a
Andrew Leifer <leifer@princeton.edu>
2019-01-09 15:43:25 -0500

    draft update of positions

--- positions.php
+++ positions.php
@@ -12,7 +12,7 @@
 	<!-- <p>We'll get back to you on that...</p> -->
 	</div>
 	<div class="meaty">
-	<p>Our work spans disciplines. We seek individuals with backgrounds in physics, neuroscience, molecular biology, engineering or programming who are interested in applying their skills towards investigating how patterns of neural activity drive behavior.</p>
+	<p>We seek individuals with backgrounds in physics, neuroscience, molecular biology, engineering or computer science who are interested in applying their skills towards investigating how patterns of neural activity drive behavior.</p>
 <!--	<p>We are seeking people at all levels:
 		<ul>
 			<li><a href="#grad">Graduate Student</a></li>
@@ -27,20 +27,34 @@
 <!-- <p>Future searches and openings will be posted here.</p> -->
 
 <h2><a name="grad"></a>Graduate Students</h2>
-<p>Prospective graduate students should apply directly to Princeton's doctoral programs in either <a href="https://www.princeton.edu/physics/graduate-program/">Physics</a>, <a href="https://pni.princeton.edu/graduate-program/ph.d.-neuroscience">Neuroscience</a> or <a href="http://lsi.princeton.edu/qcbgraduate/">Quantitative and Computational Biology</a>. Students from other Princeton doctoral programs may be elibiglbe to conduct research in the lab on a case-by-case basis.
-Current or incoming Princeton graduate students interested in rotating in the lab should send a CV to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting. </p>
+<p>Prospective grad students should apply to Princeton's PhD programs in either <a href="https://www.princeton.edu/physics/graduate-program/">Physics</a>, <a href="https://pni.princeton.edu/graduate-program/ph.d.-neuroscience">Neuroscience</a> or <a href="http://lsi.princeton.edu/qcbgraduate/">Quantitative and Computational Biology</a>.
+Current or incoming Princeton graduate students interested in rotating should send a CV to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting. </p>
 <hr />
 
 
 <h2> <a name="undergrad"></a>Undergraduates</h2>
-<p>Princeton undergraduates interested in conducting research in the summer or as part of a Junior or Senior Thesis should send a CV to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a visit. Please note: there are many funding opportunities for undergraduates, but they often have deadlines many months in advance, so please get in touch early. For example, our <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">Summer Undergraduate Research Program in Molecular, Quantitative and Computational Biology</a> has a deadline of February 1st.</p>
+<p>Princeton undergraduates interested in summer research or in conducting a  Junior or Senior Thesis should send a CV to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting. It helps to get in touch early.</p> 
+	
+<p>Undergraduates from other instituions should consider applying to the <a href="http://molbio.princeton.edu/undergraduate/research/summer-research">MOL/QCB SURP</a> or <a href="http://pni.princeton.edu/summer-programs/pni-summer-internship-program">PNI Summer</a> research programs. Applications are usually due February 1.
+	
+	
 <hr />
 
 
 
 
 <h2><a name="postdoc"></a>Post-docs</h2>
-The lab currently has no posted openings for post-doctoral positions. Nonetheless, it never hurts to contact Andrew Leifer directly at <a href="mailto:leifer@princeton.edu">leifer@princeton.edu</a>.
+<p>We are currently recruiting postdocs with a background in systems and circuit-level neuroscience. We expect to post a formal job ad  shortly. In the meantime, feel free to send a CV, cover letter or research statement, and contact info for three references to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> directly.</p>
+
+
+<p>
+Exceptional postdoctoral candidates interested in working or collaborating with the lab are further encouraged to apply to any of the following prestigious named fellowships. These are awarded competitively at the departmental level and provide additional recognition and benefits. Check the job listings for up-to-date  deadlines, and please notify Andrew Leifer if you plan to apply.
+<ul>
+<li>The CPBF Fellowship at the Center for Physics of Biological Function. Deadline is usually early November.</li>
+<li>The CV Starr Fellowship at the Princeton Neuroscience Institute. Deadline is usually late January.</li>
+<li>The Robert Dicke Fellowship at the Department of Physics.  Deadline is usually mid-November.      </li>
+</ul>
+</p>
 
 
 </div>
