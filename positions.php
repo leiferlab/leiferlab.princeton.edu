5d738bbbfd988f738e994676e98f9ae10bc80f3b
Andrew Leifer <leifer@princeton.edu>
2020-05-22 16:08:03 -0400

    added research professional position

--- positions.php
+++ positions.php
@@ -25,6 +25,51 @@
 <!-- <h3>There are currently no ongoing searches</h3> -->
 
 <!-- <p>Future searches and openings will be posted here.</p> -->
+<h2><a name="professional"></a>Research Specialist</h2>
+<p>A Research Specialist is sought to perform nematode transgenics in support of research efforts in the Leifer Lab. The Leifer Lab studies patterns of neural activity that drive behavior in the nematode C. elegans using novel imaging approaches that combine techniques from physics, neuroscience and molecular biology.</p> 
+
+Specific duties include:
+<ul>
+    <li>Maintaining C. elegans stocks for experimentation.</li>
+    <li>Performing genetic crosses.</li>
+    <li>Screening transgenic animals.</li>
+    <li>Performing molecular cloning and plasmid design.</li>
+    <li>Microinjecting C. elegans for creating new transgenic nematode strains.</li>
+    <li>Preparing media, pouring plates, and washing glassware.</li>
+    <li>Preparing, organizing and purchasing equipment, supplies and reagents.</li>
+    <li>Other support duties to facilitate a safe, productive and efficient lab environment.</li>
+    <li>Conducting occasional laboratory experiments in support of others and carefully managing and recording experimental conditions.</li>
+    <li>    Recording and analyzing fluorescent microscopy images and videos.</li>
+    <li>Familiarize her or himself with the current literature and projects of the lab.</li>
+    <li>Perform other related duties as assigned.</li>
+</ul>
+
+ESSENTIAL Qualificiations:
+<ul>
+    <li>BA or BS in biology, neuroscience, bioengineering or related field.</li>
+    <li>Prior experience with genetics, molecular cloning and PCR.</li>
+    <li>Prior experience working with an invertebrate genetic model system.</li>
+    <li>Ability to conduct laboratory experiments independently and carefully manage and record experimental conditions.</li>
+    <li>Ability to maintain a meticulous daily laboratory notebook.</li>
+    <li>Ability to work well with others.</li>
+    <li>Ability to read and extract information from primary scientific literature.</li>
+    <li>Excellent computer skills with proficiency in Microsoft Word, Excel and PowerPoint.</li>
+    <li>Applicant must have excellent organizational skills and flexibility to handle multiple tasks simultaneously.</li>
+    <li>The applicant should have a strong attention to detail and an independent problem-solving mentality.</li>
+</ul>
+
+PREFERRED Qualifications:
+<ul>
+    <li>Prior experience working with C. elegans.</li>
+    <li>Prior experience with microinjection.</li>
+    <li>Prior experience with Gibson cloning techniques.</li>
+</ul>
+
+Princeton University is an Equal Opportunity/Affirmative Action Employer and all qualified applicants will receive consideration for employment without regard to age, race, color, religion, sex, sexual orientation, gender identity or expression, national origin, disability status, protected veteran status, or any other characteristic protected by law. EEO IS THE LAW
+	
+Apply online through Princeton's job site at <a href="https://social.icims.com/viewjob/po15901760129970a207">https://social.icims.com/viewjob/po15901760129970a207</a>.
+
+
 
 <h2><a name="grad"></a>Graduate Students</h2>
 <p>Prospective grad students should apply to Princeton's PhD programs in either <a href="https://www.princeton.edu/physics/graduate-program/">Physics</a>, <a href="https://pni.princeton.edu/graduate-program/ph.d.-neuroscience">Neuroscience</a> or <a href="http://lsi.princeton.edu/qcbgraduate/">Quantitative and Computational Biology</a>.
@@ -45,11 +90,8 @@ Current or incoming Princeton graduate students interested in rotating should se
 
 <h2><a name="postdoc"></a>Post-docs</h2>
 
-<p>We are seeking an extremely motivated postdoctoral researcher with a background in neural systems or circuits investigations. The postdoctoral researcher will leverage the lab's optical neurophysiology toolkit to investigate neural circuit function underlying behavior in the nematode C. elegans. Experimental approaches will include calcium imaging of brain activity, optogenetic manipulation of specific neurons and circuits, behavioral experiments and genetic studies. Possible application domains may include sensorimotor processing, chemosensory guided navigation, mating behavior or learning. </p>
+<p> The Leifer Lab has no specific post-doc search at this time.  Please feel free to reach out to Adnrew Leifer directly if you are a grad student about to graduate. </p>
 
-
-<p>Interested applicants must apply online at <a href="https://www.princeton.edu/acad-positions/position/10281">https://www.princeton.edu/acad-positions/position/10281</a>.</p>
-</p>
 
 <h2><a name="postdocfellowships"></a>Post-doc Fellowships</h2>
 
