8dbc67b1d157930a0b42c03d39a4447ca01ac692
Andrew Leifer <leifer@princeton.edu>
2020-08-26 20:49:18 +0200

    removed open search

--- positions.php
+++ positions.php
@@ -25,50 +25,6 @@
 <!-- <h3>There are currently no ongoing searches</h3> -->
 
 <!-- <p>Future searches and openings will be posted here.</p> -->
-<h2><a name="professional"></a>Research Specialist</h2>
-<p>A Research Specialist is sought to perform nematode transgenics in support of research efforts in the Leifer Lab. The Leifer Lab studies patterns of neural activity that drive behavior in the nematode C. elegans using novel imaging approaches that combine techniques from physics, neuroscience and molecular biology.</p> 
-
-Specific duties include:
-<ul>
-    <li>Maintaining C. elegans stocks for experimentation.</li>
-    <li>Performing genetic crosses.</li>
-    <li>Screening transgenic animals.</li>
-    <li>Performing molecular cloning and plasmid design.</li>
-    <li>Microinjecting C. elegans for creating new transgenic nematode strains.</li>
-    <li>Preparing media, pouring plates, and washing glassware.</li>
-    <li>Preparing, organizing and purchasing equipment, supplies and reagents.</li>
-    <li>Other support duties to facilitate a safe, productive and efficient lab environment.</li>
-    <li>Conducting occasional laboratory experiments in support of others and carefully managing and recording experimental conditions.</li>
-    <li>    Recording and analyzing fluorescent microscopy images and videos.</li>
-    <li>Familiarize her or himself with the current literature and projects of the lab.</li>
-    <li>Perform other related duties as assigned.</li>
-</ul>
-
-ESSENTIAL Qualificiations:
-<ul>
-    <li>BA or BS in biology, neuroscience, bioengineering or related field.</li>
-    <li>Prior experience with genetics, molecular cloning and PCR.</li>
-    <li>Prior experience working with an invertebrate genetic model system.</li>
-    <li>Ability to conduct laboratory experiments independently and carefully manage and record experimental conditions.</li>
-    <li>Ability to maintain a meticulous daily laboratory notebook.</li>
-    <li>Ability to work well with others.</li>
-    <li>Ability to read and extract information from primary scientific literature.</li>
-    <li>Excellent computer skills with proficiency in Microsoft Word, Excel and PowerPoint.</li>
-    <li>Applicant must have excellent organizational skills and flexibility to handle multiple tasks simultaneously.</li>
-    <li>The applicant should have a strong attention to detail and an independent problem-solving mentality.</li>
-</ul>
-
-PREFERRED Qualifications:
-<ul>
-    <li>Prior experience working with C. elegans.</li>
-    <li>Prior experience with microinjection.</li>
-    <li>Prior experience with Gibson cloning techniques.</li>
-</ul>
-
-<p>Princeton University is an Equal Opportunity/Affirmative Action Employer and all qualified applicants will receive consideration for employment without regard to age, race, color, religion, sex, sexual orientation, gender identity or expression, national origin, disability status, protected veteran status, or any other characteristic protected by law. EEO IS THE LAW</p>
-	
-<p>Apply online through Princeton's job site at <a href="https://social.icims.com/viewjob/po15901760129970a207">https://social.icims.com/viewjob/po15901760129970a207</a>.</p>
-<hr />
 
 
 <h2><a name="grad"></a>Graduate Students</h2>
@@ -80,7 +36,7 @@ Current or incoming Princeton graduate students interested in rotating or doing 
 <h2> <a name="undergrad"></a>Undergraduates</h2>
 <p>Princeton undergraduates interested in summer research or in conducting a  Junior or Senior Thesis should send a CV to <a href="mailto:leifer@princeton.edu">Andrew Leifer</a> to schedule a meeting. It helps to get in touch early.</p> 
 	
-<p>Undergraduates from other instituions should consider applying to the <a href="https://molbio.princeton.edu/undergraduate/research/surp">MOL/QCB SURP</a> or <a href="http://pni.princeton.edu/summer-programs/pni-summer-internship-program">PNI Summer</a> research programs. Applications are usually due February 1.
+<p>Undergraduates from other institutions should consider applying to the <a href="https://molbio.princeton.edu/undergraduate/research/surp">MOL/QCB SURP</a> or <a href="http://pni.princeton.edu/summer-programs/pni-summer-internship-program">PNI Summer</a> research programs. In prior years applications have been due February 1.
 	
 	
 <hr />
@@ -90,7 +46,7 @@ Current or incoming Princeton graduate students interested in rotating or doing 
 
 <h2><a name="postdoc"></a>Post-docs</h2>
 
-<p> The Leifer Lab has no specific post-doc search at this time.  Please feel free to reach out to Adnrew Leifer directly if you are a grad student about to graduate. </p>
+<p> The Leifer Lab has no specific post-doc search at this time.  Please feel free to reach out to Andrew Leifer directly if you are a grad student about to graduate. </p>
 
 
 <h2><a name="postdocfellowships"></a>Post-doc Fellowships</h2>
@@ -98,7 +54,7 @@ Current or incoming Princeton graduate students interested in rotating or doing 
 <p>
 Exceptional postdoctoral candidates interested in working or collaborating with the lab are further encouraged to apply to any of the following prestigious named fellowships. These are awarded competitively at the departmental level and provide additional recognition and benefits. Please also notify Andrew Leifer if you plan to apply.
 <ul>
-<li><a href="https://biophysics.princeton.edu/opportunit">CPBF Fellowship</a> at the <a href="https://biophysics.princeton.edu/">Center for Physics of Biological Function</a>. Deadline is usually early November.</li>
+<li><a href="https://biophysics.princeton.edu/opportunit">CPBF Fellowship</a> at the <a href="https://biophysics.princeton.edu/">Center for Physics of Biological Function</a>. Deadline is usually October or November.</li>
 <li><a href="http://pni.princeton.edu/opportunities/cv-starr-fellow-opportunities">CV Starr Fellowship</a> at the <a href="http://pni.princeton.edu">Princeton Neuroscience Institute</a>. Deadline is usually late January.</li>
 <li><a href="https://phy.princeton.edu/dicke-fellows">Robert Dicke Fellowship</a> at the <a href="http://phy.princeton.edy">Department of Physics</a>.  Deadline is usually mid-November.      </li>
 </ul>
