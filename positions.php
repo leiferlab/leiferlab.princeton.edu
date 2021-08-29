3bee3fead08617cfbac025f7b633e067db3ff010
Andrew Leifer <leifer@princeton.edu>
2021-08-28 20:48:58 -0400

    draft of 2 posdoc adds

--- positions.php
+++ positions.php
@@ -44,12 +44,60 @@ Current or incoming Princeton graduate students interested in rotating or doing 
 
 
 
-<h2><a name="postdoc"></a>Post-docs</h2>
+<h2><a name="postdoc"></a>Post-doctoral positions</h2>
 
-<p> The Leifer Lab has no specific post-doc search at this time.  Please feel free to reach out to Andrew Leifer directly if you are a grad student about to graduate. </p>
+<h3>Precision measurements of the brain</h3>
+
+<p>A postdoctoral research associate or more senior researcher is sought for a neural imaging and optogenetics project in moving animals. A PhD in a related field is required as well as a background in experimental physics, optics, computer science, electrical engineering or neuroscience with expertise building and using custom scientific instruments for precision measurements. In particular, someone is sought who is comfortable working with custom optics, ultrafast lasers, electronics and custom software. Candidates should be self-motivated by a desire to understand open questions in neuroscience.</p>
+
+<p>Ideal attributes are:
+<ul>
+<li>Talented experimentalist with a history of performing difficult measurements </li>
+<li>Motivated by an interest in neuroscience</li>
+<li>Talented troubleshooter of hardware and software</li>
+<li>Experience with non-linear optics, lasers and fundamentals of microscope design</li>
+<li>Fluency in at least one programming language, and prior exposure to Python and LabView. </li>
+<li>Able and willing to dive into large code-bases </li>
+<li>Experience with digital signal processing and image processing</li>
+<li>Comfortable with mathematical concepts and manipulations</li>
+<li>Comfortable learning new topics independently via the literature in the areas of computer vision, machine learning, optics or electronics </li>
+<li>Ability to mentor others</li>
+<li>Organized</li>
+<li>Able to work well in a team, but also able to work independently</li>
+<li>Clear written and oral communicator</li>
+</ul>
+</p>
+
+<p>Apply for this position online via Princeton's A-Hire system at: <a href="https://www.princeton.edu/acad-positions/position/21462"> https://www.princeton.edu/acad-positions/position/21462</a>.</p>
 
 
-<h2><a name="postdocfellowships"></a>Post-doc Fellowships</h2>
+<h3>Neural circuits and behavior in <i>C. elegans</i></h3>
+
+<p>We are seeking an extremely motivated postdoctoral researcher with a background in neural systems or circuits investigations. The postdoctoral researcher will leverage the lab's optical neurophysiology toolkit to investigate neural circuit function underlying behavior in the nematode C. elegans. Experimental approaches will include brain-wide population calcium imaging of brain activity, optogenetic manipulation of specific neurons and circuits, large-scale measurements of functional connectivity, behavioral experiments, genetic studies and measures of gene expression. Possible application domains may include sensorimotor processing, chemosensory guided navigation, mating behavior or learning. </p>
+
+<p>A PhD in neuroscience, molecular biology, biophysics or a related field is required. Successful applicants will have demonstrated a strong record of research accomplishment and creativity. Appointments are for one year, renewable annually based on satisfactory performance and availability of funding.</p>
+
+<p>Ideal attributes are:
+<ul>=
+<li>Experience with systems and circuit-level investigations involving, neural dynamics, genes and behavior</li>
+<li>Talented troubleshooter of difficult measurements and experiments</li>
+<li>Self-motivated to follow relevant literature and advances in the field</li>
+<li>Strong quantitative skills, including familiarity applying mathematical and statistical concepts </li>
+<li>Prior experience with scientific programming in Python, MATLAB or similar. </li>
+<li>Comfortable learning new topics in data analysis, neuroscience or genetics independently via the literature </li>
+<li>Ability to mentor others</li>
+<li>Organized</li>
+<li>Able to work well in a team, but also able to work independently</li>
+<li>Clear written and oral communicator</li>
+</ul>
+</p>
+
+<p>Interested applicants must apply online at <a href="://www.princeton.edu/acad-positions/position/21861">https://www.princeton.edu/acad-positions/position/21861</a> and include a cover letter, a curriculum vitae including a publication list, and contact information of at least two references.</p>
+
+
+
+
+<h2><a name="postdocfellowships"></a>Named Post-doctoral Fellowships</h2>
 
 <p>
 Exceptional postdoctoral candidates interested in working or collaborating with the lab are further encouraged to apply to any of the following prestigious named fellowships. These are awarded competitively at the departmental level and provide additional recognition and benefits. Please also notify Andrew Leifer if you plan to apply.
