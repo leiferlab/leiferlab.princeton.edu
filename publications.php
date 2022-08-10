d7edd6893bc9b2e599acf27b83baacaaa3fed95c
Andrew Leifer <leifer@princeton.edu>
2022-08-09 21:56:27 -0700

    funconn arxiv

--- publications.php
+++ publications.php
@@ -12,7 +12,7 @@
 		</p>
 	<p>
 		<ul>
-<!--			<li><a href="#preprint">Pre peer-reviewed manuscripts</a></li> -->
+			<li><a href="#preprint">Manuscripts in the process of peer-review</a></li>
 			<li><a href="#peerreview">Peer-reviewed journal articles</a></li> 
 			<li><a href="#pop">Popular press articles featuring our work</a></li>
 			<li><a href="#vid">Video Recorded Lectures</a></li>
@@ -26,7 +26,11 @@
 	<div class="textblock">		
 		
 
-	<!--	<h3><a name="preprint"></a>Manuscripts in the process of peer-review</h3> -->
+		<h3><a name="preprint"></a>Manuscripts in the process of peer-review</h3>
+		<ul class="publications">
+			<li>Francsco Rand, Anuj K. Sharma, Sophie Dvali, Andrew M. Leifer, "A functional connectivity atlas of C. elegans measured by neural activation." <a href="https://doi.org/10.48550/arXiv.2208.04790">arXiv</a>, (2022) [<a href="https://arxiv.org/pdf/2208.04790">PDF</a>]</li>
+		</ul>
+	
 		
 </div>
 <hr /> 
