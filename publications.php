7c389d2c363d1bf1d5021432a078fb8feeca3aa2
Andrew Leifer <leifer@princeton.edu>
2025-01-02 12:00:23 -0500

    typo

--- publications.php
+++ publications.php
@@ -31,19 +31,14 @@
 			
 			<li>Rizwanul Haque, Hagar Setty, Ramiro Lorenzo, Gil Stelzer, Ron Rotkopf, Yehuda Salzberg, Gal Goldman, Sandeep Kumar, Shiraz Nir Halber, Andrew M. Leifer, Elad Schneidman, Patrick Laurent, Meital Oren-Suissa, "A Blueprint of Sex-Specific Neuronal Regulation in the C. elegans Nervous System at Single-Cell Resolution" <a href="https://doi.org/10.1101/2024.12.27.630541"><i>bioRxiv</i></a> 2024.12.27.630541, 27 Dec (2024).     [<a href="https://www.biorxiv.org/content/10.1101/2024.12.27.630541v1.full.pdf">PDF</a>]</li>
 
-			
-			
-			
-   			<li>Sophie Dvali, Caio Seguin, Richard Betzel, Andrew M. Leifer, "Diverging network architecture of the C. elegans connectome and signaling network" <a href="https://doi.org/10.48550/arXiv.2412.14498"><i>arXiv</i></a>,arXiv:2412.14498 [q-bio.NC]; 19 Dec (2024).
-    [<a href="https://arxiv.org/pdf/2412.14498">PDF</a>]</li>
-			
 
-   			<li>Sophie Dvali, Caio Seguin, Richard Betzel, Andrew M. Leifer, "Diverging network architecture of the C. elegans connectome and signaling network" <a href="https://doi.org/10.48550/arXiv.2412.14498"><i>arXiv</i></a>,arXiv:2412.14498 [q-bio.NC]; 19 Dev (2024).
+
+   			<li>Sophie Dvali, Caio Seguin, Richard Betzel, Andrew M. Leifer, "Diverging network architecture of the C. elegans connectome and signaling network" <a href="https://doi.org/10.48550/arXiv.2412.14498"><i>arXiv</i></a>, arXiv:2412.14498 [q-bio.NC]; 19 Dec (2024).
     [<a href="https://arxiv.org/pdf/2412.14498">PDF</a>]</li>
 					
-			   			<li>Matthew S Creamer, Andrew M Leifer, Jonathan W Pillow,  "Bridging the gap between the connectome and whole-brain activity in C. elegans" <a href="https://doi.org/10.1101/2024.09.22.614271"><i>bioRxiv</i></a>,2024.09.22.614271(2024).
+			   			<li>Matthew S Creamer, Andrew M Leifer, Jonathan W Pillow,  "Bridging the gap between the connectome and whole-brain activity in C. elegans" <a href="https://doi.org/10.1101/2024.09.22.614271"><i>bioRxiv</i></a>, 2024.09.22.614271(2024).
 			    [<a href="https://www.biorxiv.org/content/10.1101/2024.09.22.614271v1.full.pdf">PDF</a>]</li>	
-		   			<li>Kevin S. Chen, Anuj K. Sharma, Jonathan W. Pillow, Andrew M. Leifer, "Olfactory learning alters navigation strategies and behavioral variability in C. elegans" <a href="https://doi.org/10.48550/arXiv.2311.07117"><i>arXiv</i></a>,arXiv:2311.07117 [q-bio.NC]; 13 Nov (2023).
+		   			<li>Kevin S. Chen, Anuj K. Sharma, Jonathan W. Pillow, Andrew M. Leifer, "Olfactory learning alters navigation strategies and behavioral variability in C. elegans" <a href="https://doi.org/10.48550/arXiv.2311.07117"><i>arXiv</i></a>, arXiv:2311.07117 [q-bio.NC]; 13 Nov (2023).
 		    [<a href="https://arxiv.org/pdf/2311.07117">PDF</a>]</li>		
 		</ul>
 		
